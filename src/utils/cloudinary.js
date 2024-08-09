284b08eac80e4b6606c1c37063cd2be396fc99c8
Sanjeev Roy <sanjeev15062002@gmail.com>
2024-08-10 00:34:02 +0530

    cloudinary and multer

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,23 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+         resource_type:'auto'
+        })
+        // file has been uploaded successfull
+        console.log("File is uploaded on cloudinary", response.url);
+        return response;
+        
+    } catch (error) {
+        // remove the locally saved temporary file as the upload operation got failed
+        fs.unlinkSync(localFilePath) 
+        return null;
+    }
+}
+
+export default uploadOnCloudinary
