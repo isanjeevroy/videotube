ea3e24c800f8fe61dec70f0badaa7a18a77a5ce1
Sanjeev Roy <sanjeev15062002@gmail.com>
2024-08-11 19:26:41 +0530

    fixed bug in route updating images

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
-import {loginUser, logoutUser, registerUser, refreshAccessToken} from "../controllers/user.controller.js"
-import {upload} from "../middlewares/multer.middleware.js"
+import { loginUser, logoutUser, registerUser, refreshAccessToken, updateAccountDetails, updateUserAvatar, updateCoverImage } from "../controllers/user.controller.js"
+import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router = Router()
@@ -8,12 +8,12 @@ const router = Router()
 router.route("/register").post(
     upload.fields([
         {
-            name:"avatar",
-            maxCount:1
+            name: "avatar",
+            maxCount: 1
         },
         {
-            name:"coverImage",
-            maxCount:1
+            name: "coverImage",
+            maxCount: 1
         }
     ]),
     registerUser
@@ -23,7 +23,19 @@ router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
+
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/update-profile").post(verifyJWT, updateAccountDetails)
+
+router.route("/update-avatar").post(
+    verifyJWT,
+    upload.single("avatar"),
+    updateUserAvatar)
+router.route("/update-cover-image").post(
+    verifyJWT,
+    upload.single("coverImage"),
+    updateCoverImage)
 
 
 export default router
