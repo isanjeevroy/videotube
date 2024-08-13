5735f8a13a19bb53dda58bc6226dc1786d42b558
Sanjeev Roy <sanjeev15062002@gmail.com>
2024-08-13 18:48:49 +0530

    add user controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,40 +1,195 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Video} from "../models/video.models.js"
-import {User} from "../models/user.models.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import mongoose, { isValidObjectId } from "mongoose"
+import { Video } from "../models/video.models.js"
+import { User } from "../models/user.models.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+
     //TODO: get all videos based on query, sort, pagination
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
-    // TODO: get video, upload to cloudinary, create video
+
+    //get data
+    const { title, description } = req.body
+
+    //validate data
+    if (!title && !description) {
+        throw new ApiError(400, "All fields are required!")
+    }
+
+    //get files from multer
+    const localFileVideo = req.files?.videoFile[0]?.path;
+    const localFileThumbnail = req.files?.thumbnail[0]?.path;
+
+    //validate files
+    if (!localFileVideo && !localFileThumbnail) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    // uploaded to cloudinary
+    const videoFile = await uploadOnCloudinary(localFileVideo)
+    const thumbnail = await uploadOnCloudinary(localFileThumbnail)
+
+    // validate uploaded files
+    if (!videoFile && !thumbnail) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    // duration of video from cloudinary
+    const duration = parseFloat(videoFile.duration.toFixed(2))
+
+    // create in database
+    const video = await Video.create({
+        videoFile: videoFile.url,
+        thumbnail: thumbnail.url,
+        title,
+        description,
+        duration,
+        owner: req.user?._id
+    })
+
+    //send response
+    return res
+        .status(200)
+        .json(new ApiResponse(200, video, "Video has uploaded successfully!"))
+
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
+
+    //get videoId
     const { videoId } = req.params
-    //TODO: get video by id
+
+    //validate videoId
+    if (!videoId) {
+        throw new ApiError(400, "Video Id is missing!")
+    }
+
+    //find video by id
+    const video = await Video.findById(videoId)
+
+    //validate video
+    if (!video) {
+        throw new ApiError(400, "Does not exist video for this id!")
+    }
+
+    //return response
+    return res
+        .status(200)
+        .json(new ApiResponse(200, video, "Video fetched successfully!"))
+
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
+
+    //get video id
     const { videoId } = req.params
-    //TODO: update video details like title, description, thumbnail
+
+    const { title, description } = req.body
+
+    //validate data
+    if (!title && !description) {
+        throw new ApiError(400, "All field are required!")
+    }
+
+    // find path
+    const localFileThumbnail = req.file?.path
+
+    // validate thumbnail
+    if (!localFileThumbnail) {
+        throw new ApiError(400, "Thumbanil is missing!")
+    }
+
+    // upload to cloudinary
+    const thumbnail = await uploadOnCloudinary(localFileThumbnail)
+
+    // validate thumbnail
+    if (!thumbnail) {
+        throw new ApiError(500, "Error occures while uploading the video!")
+    }
+
+    //update in database
+    const video = await Video.findByIdAndUpdate(videoId, {
+        $set: {
+            title,
+            description,
+            thumbnail: thumbnail.url
+        }
+    },
+        { new: true }
+    )
+
+    // return response
+    return res
+        .status(200)
+        .json(new ApiResponse(200, video, "Video has updated successfully!"))
 
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
+
+    //get video id
     const { videoId } = req.params
-    //TODO: delete video
+
+    // validate id
+    if (!videoId) {
+        throw new ApiError(400, "Video's id is missing!")
+    }
+
+    //find the video exist or not
+    const video = await Video.findByIdAndDelete(videoId)
+
+    // validate video
+    if (!video) {
+        throw new ApiError(400, "Doesn't exist video!")
+    }
+
+    // return response
+    return res
+        .status(200)
+        .json(new ApiResponse(200, video, "Video deleted successfully!"))
+
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
+
+    //get video id
     const { videoId } = req.params
+
+    //validate video id
+    if (!videoId) {
+        throw new ApiError(400, "Video id is missing!")
+    }
+
+    //find video
+    const video = await Video.findById(videoId)
+
+     // validate video
+     if (!video) {
+        throw new ApiError(400, "Video not found!")
+    }
+
+    //update video
+    const updateVideo = await Video.findByIdAndUpdate(videoId,
+        {
+            $set:{
+                isPublished: !video.isPublished 
+            }
+        },
+        { new: true }
+    )
+
+    //return response
+    return res
+    .status(200)
+    .json(new ApiResponse(200, updateVideo, "Video is updated successfully!"))
+
 })
 
 export {
--- src/models/video.models.js
+++ src/models/video.models.js
@@ -4,11 +4,11 @@ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const videoSchema = new Schema(
     {
         videoFile:{
-            type: String, // cloudinary url
+            type: String, 
             required: true,
         },
         thumbnail:{
-            type: String, // cloudinary url
+            type: String, 
             required: true,
         },
         title:{
