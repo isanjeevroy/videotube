da778a0b39ffb3e64f6e076994ab98508a7fd0cc
Sanjeev Roy <sanjeev15062002@gmail.com>
2024-08-15 14:30:22 +0530

    modify in subscription route

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -5,8 +5,8 @@ import {verifyJWT} from "../middlewares/auth.middleware.js"
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/c/:channelId").get(getSubscribedChannels).post(toggleSubscription);
+router.route("/u/:subscriberId").get(getSubscribedChannels).post(toggleSubscription);
 
-router.route("/u/:subscriberId").get(getUserChannelSubscribers);
+router.route("/c/:channelId").get(getUserChannelSubscribers);
 
 export default router
